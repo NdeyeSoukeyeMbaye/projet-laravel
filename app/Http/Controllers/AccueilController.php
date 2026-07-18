9104413acc8394e6661e248aed59ab9b74578be7
diarra478 <diarraboussa2022@gmail.com>
2026-07-18 21:22:54 +0200

    Description des modifications

---
+++ app/Http/Controllers/AccueilController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AccueilController extends Controller
+{
+     public function index()
+    {
+        $profils = [
+            [
+                'nom' => 'Patient',
+                'description' => 'Consulter mes rendez-vous et mon dossier médical',
+                'route' =>'/'
+            ],
+            [
+                'nom' => 'Médecin',
+                'description' => 'Gérer mon planning et mes patients',
+                'route' => '/'
+            ],
+            [
+                'nom' => 'Secrétaire',
+                'description' => 'Accueil, paiements, rappels et admissions',
+                'route' => '/'
+            ],
+            [
+                'nom' => 'Administrateur',
+                'description' => 'Gestion des utilisateurs et statistiques',
+                'route' => '/'
+            ],
+            [
+                'nom' => 'Super Admin',
+                'description' => 'Administration complète et sécurité',
+                'route' => '/'
+            ],
+        ];
+
+        return view('welcome', compact('profils'));
+    }
+}
